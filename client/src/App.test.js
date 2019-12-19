58a9111a2b8349fd2ab1b16fb1ef663304c1eba0
Joerg Vossmann <joerg@JoergsMacBookPro.local>
2019-12-19 15:10:13 +0100

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {});
+// delete because the fib.js would crash so the backend server doesnot exsist at this point in time
+//  const div = document.createElement('div');
+//  ReactDOM.render(<App />, div);
+//  ReactDOM.unmountC omponentAtNode(div);
+//});
